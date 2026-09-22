5317a6b3743e3cc2d251b3d1b7ed1ae308e44f48
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-09-22 20:06:47 +0800

    models done

--- films/admin.py
+++ films/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from films.models import Film, Genre, Review, Director, Actor
 
-# Register your models here.
-"""
-from students.models import Student
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ["name", "release_date", "director"]
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'group_id']
-"""
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_name = ["name"]
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["film", "author", "rate"]
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ["name", "birth_date"]
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_dispalay = ["name", "birth_date"]
--- films/models.py
+++ films/models.py
@@ -6,27 +6,48 @@ class Film(models.Model):
     rating = models.IntegerField("Рейтинг")
     release_date = models.DateField("Дата выхода")
 
-    director = models.ForeignKey("Director", on_delete=models.CASCADE, verbose_name="Режиссер")
+    director = models.ForeignKey("Director", on_delete=models.CASCADE, null=True)
+    genre = models.ForeignKey("Genre", on_delete=models.CASCADE)
     actors = models.ManyToManyField("Actor", related_name="films")
-    reviews = models.ForeignKey("Review", on_delete=models.CASCADE, verbose_name="Рецензия")
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
 
     def __str__(self) -> str:
         return self.name
 
-class Genres(models.Model):
+class Genre(models.Model):
     name = models.TextField("Название")
 
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
 class Review(models.Model):
+    film = models.ForeignKey("Film", on_delete=models.CASCADE, null=True)
     review_text = models.TextField("Текст рецензии")
     author = models.TextField("Автор")
     rate = models.IntegerField("Оценка")
 
+    class Meta:
+        verbose_name = "Рецензия"
+        verbose_name_plural = "Рецензии"
+
 class Director(models.Model):
     name = models.TextField("ФИО")
     birth_date = models.DateField("Дата рождения", null=True, blank=True)
-    biofraphy = models.TextField("Биография", null=True, blank=True)
+    biography = models.TextField("Биография", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Режиссер"
+        verbose_name_plural = "Режиссеры"
 
 class Actor(models.Model):
     name = models.TextField("ФИО")
     birth_date = models.DateField("Дата рождения", null=True, blank=True)
     biography = models.TextField("Биография", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Актер"
+        verbose_name_plural = "Актеры"
